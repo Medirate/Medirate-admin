1a94a431661bea88398f7c9578417fd812e34f8c
Medirate <medirate.net@gmail.com>
2025-06-25 06:03:20 +0530

    update

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { DataProvider } from "@/context/DataContext";
 import { useState } from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
@@ -9,9 +8,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <DataProvider>
-        {children}
-      </DataProvider>
+      {children}
     </QueryClientProvider>
   );
 } 
